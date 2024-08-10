518c9e6e4942ad77eee148e0c760d626575c8d82
nursecat <178041924+nursecat@users.noreply.github.com>
2024-08-10 13:48:29 -0600

    initial code commit

---
+++ ears/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace ears;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, ears!");
+    }
+}
---
+++ the beans/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace the_beans;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, beans!");
+    }
+}
