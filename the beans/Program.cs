6c03227795c425de98aa02bb306ecd89e4d6c097
nursecat <178041924+nursecat@users.noreply.github.com>
2024-08-10 14:15:42 -0600

    tolower input

--- the beans/Program.cs
+++ the beans/Program.cs
@@ -5,5 +5,8 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, beans!");
+        Console.WriteLine("enter text");
+        var line = Console.ReadLine()!;
+        Console.WriteLine("Your text: " + line.ToLower());
     }
 }
